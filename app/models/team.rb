7afc8eacf6b359721a5177c81da6dc8c9900af1d
Sam Ferebee <sam.ferebee@gmail.com>
2021-04-08 16:21:51 -0400

    user account stuff

--- app/models/team.rb
+++ app/models/team.rb
@@ -18,4 +18,21 @@ class Team < ApplicationRecord
     belongs_to :cb
     belongs_to :fs
     belongs_to :ss
+
+    def offense_rating
+        total = self.quarterback.overall + self.runningback.overall + self.wide_receiver.overall + self.tightend.overall + self.ltackle.overall + self.lguard.overall + self.center.overall + self.rguard.overall + self.rtackle.overall
+        total / 9
+    end
+
+    def defense_rating
+        total = self.leftend.overall + self.rightend.overall + self.dtackle.overall + self.lolb.overall + self.rolb.overall + self.mlb.overall + self.cb.overall + self.ss.overall + self.fs.overall
+        total / 9
+    end
+
+    def overall_rating
+        (offense_rating + defense_rating) / 2
+    end
+
+
+
 end
